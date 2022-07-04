8a421a0308e3f42f1153ea00975ef8537460146b
Abe Chen <abe.chen@farmland.tw>
2022-07-04 11:49:09 +0800

    Initial Commit

---
+++ FoodpinHomework/FoodpinHomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodpinHomeworkApp.swift
+//  FoodpinHomework
+//
+//  Created by abe chen on 2022/7/4.
+//
+
+import SwiftUI
+
+@main
+struct FoodpinHomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
